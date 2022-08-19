02305fe4e103587e83a7e33852b05f2f8caf2223
Cristian Pavliuc <cristian.pavliuc@boohoo.com>
2022-08-19 15:01:39 +0300

    Project crated

---
+++ CryptoApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by Cristian Pavliuc on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
